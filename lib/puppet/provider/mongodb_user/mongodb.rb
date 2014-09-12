645b8569ae758247409012e3987a28008e5aee9d
Marc Dutoo <marc.dutoo@openwide.fr>
2014-09-12 18:03:46 +0200

    MongoDB 2.6 - user provider : also patched destroy, password*,    somehow set roles, started work to patch others

--- lib/puppet/provider/mongodb_user/mongodb.rb
+++ lib/puppet/provider/mongodb_user/mongodb.rb
@@ -17,10 +17,11 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
   end
 
   def create
-#Puppet.warning(@resource.parent.parent.instance_variables.map{|var| puts [var, @resource.parent.parent.instance_variable_get(var)].join(":")})
+    # TODO switch according to MongoDB version :
+    #Puppet.warning(@resource.parent.parent.instance_variables.map{|var| puts [var, @resource.parent.parent.instance_variable_get(var)].join(":")})
     # before MongoDB 2.6 :
     #mongo(@resource[:database], '--eval', "db.system.users.insert({user:\"#{@resource[:name]}\", pwd:\"#{@resource[:password_hash]}\", roles: #{@resource[:roles].inspect}})")
-    # since MongoDB 2.6, users are stored in admin db and credentials and roles are more complex
+    # since MongoDB 2.6, users are stored in admin db and credentials and roles are more complex :
     roleMaps = []
     @resource[:roles].each { |role| roleMaps << {"role"=>role,"db"=>@resource[:database]} }
     require 'json'
@@ -28,28 +29,50 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb) do
   end
 
   def destroy
-    mongo(@resource[:database], '--quiet', '--eval', "db.removeUser(\"#{@resource[:name]}\")")
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.removeUser(\"#{@resource[:name]}\")")
+    # since MongoDB 2.6, users are stored in admin db and removeUser is deprecated :
+    mongo("admin", '--quiet', '--eval', "db.dropUser(\"#{@resource[:name]}\")")
   end
 
   def exists?
     block_until_mongodb(@resource[:tries])
-    mongo(@resource[:database], '--quiet', '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
+    # TODO since MongoDB 2.6, users are stored in admin db : TODO $elemMatch on user & roles.db
+    mongo("admin", '--quiet', '--eval', "db.system.users.find({user:\"#{@resource[:name]}\"}).count()").strip.eql?('1')
   end
 
   def password_hash
-    mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"pwd\"]").strip
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"pwd\"]").strip
+    # since MongoDB 2.6, users are stored in admin db :
+    mongo("admin", '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"credentials\"][\"MONGODB-CR\"]").strip
   end
 
   def password_hash=(value)
-    mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {pwd:\"#{value}\"}})")
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {pwd:\"#{value}\"}})")
+    # since MongoDB 2.6, users are stored in admin db :
+    mongo("admin", '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {credentials:{\"MONGODB-CR\":\"#{value}\"}}})")
   end
 
   def roles
-    mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"]").strip.split(",").sort
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"]").strip.split(",").sort
+    # TODO since MongoDB 2.6, users are stored in admin db : TODO rather on roles.db, filter out false matches, map to array, or even ::db.roles to map
+    mongo("admin", '--quiet', '--eval', "db.system.users.findOne({user:\"#{@resource[:name]}\"})[\"roles\"]").strip.split(",").sort
   end
 
   def roles=(value)
-    mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {roles: #{@resource[:roles].inspect}}})")
+    # before MongoDB 2.6 :
+    #mongo(@resource[:database], '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {roles: #{@resource[:roles].inspect}}})")
+    # since MongoDB 2.6, users are stored in admin db and credentials and roles are more complex :
+    roleMaps = []
+    @resource[:roles].each { |role| roleMaps << {"role"=>role,"db"=>@resource[:database]} }
+    require 'json'
+    # TODO or on \"_id\":\"#{@resource[:database]}.#{@resource[:name]}\" instead of user:\"#{@resource[:name]}\"} ?
+    mongo("admin", '--quiet', '--eval', "db.system.users.update({user:\"#{@resource[:name]}\"}, { $set: {roles: #{roleMaps.to_json}}})")
   end
 
 end
